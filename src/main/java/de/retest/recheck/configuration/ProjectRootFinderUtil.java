da4629fd279af53f9f9d3afdd73e6f8a31947833
Enes Özel <enes.oezel@retest.de>
2019-02-20 15:48:26 +0100

    Improve log instead of throw exception

--- src/main/java/de/retest/recheck/configuration/ProjectRootFinderUtil.java
+++ src/main/java/de/retest/recheck/configuration/ProjectRootFinderUtil.java
@@ -1,16 +1,19 @@
 package de.retest.recheck.configuration;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Sets;
 
 public class ProjectRootFinderUtil {
 
-	private static Set<ProjectRootFinder> projectRootFinder = Sets.newHashSet( new MavenProjectRootFinder() );
+	private static final Logger logger = LoggerFactory.getLogger( ProjectRootFinder.class );
+
+	private static final Set<ProjectRootFinder> projectRootFinder = Sets.newHashSet( new MavenProjectRootFinder() );
 
 	private ProjectRootFinderUtil() {
 
@@ -22,8 +25,7 @@ public class ProjectRootFinderUtil {
 				.filter( Optional::isPresent ) //
 				.map( Optional::get ) //
 				.findAny() //
-				.orElseThrow( () -> new UncheckedIOException(
-						new IOException( "Project root not found in current workdir or any parent folder." ) ) );
+				.orElse( noProjectRootFoundWarning() );
 	}
 
 	public static Path getProjectRoot( final Path basePath ) {
@@ -32,8 +34,12 @@ public class ProjectRootFinderUtil {
 				.filter( Optional::isPresent ) //
 				.map( Optional::get ) //
 				.findAny() //
-				.orElseThrow( () -> new UncheckedIOException(
-						new IOException( "Project root not found in " + basePath + " or any parent folder." ) ) );
+				.orElse( noProjectRootFoundWarning( basePath ) );
 	}
 
+	private static Path noProjectRootFoundWarning( final Path... basePath ) {
+		logger.warn( "Project root not found in {} or any parent folder.",
+				basePath.length < 1 ? "current workdir" : basePath );
+		return null;
+	}
 }
